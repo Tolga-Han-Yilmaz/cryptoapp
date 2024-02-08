3e446d65712d4efbd46d244a7151b3ab25169167
Tolga Han Yılmaz <101406062+Tolga-Han-Yilmaz@users.noreply.github.com>
2024-02-08 22:45:12 +0100

    components created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router";
+import { Layout, Typography, Space } from "antd";
+
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
+import "./App.css";
 
 const App = () => {
-  return <div>
-  <h1/>crypto</div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
 };
 
 export default App;
